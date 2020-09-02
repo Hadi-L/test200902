76d2609215dd09d8ee9a58a448bf07d95c6c3a25
Hadi-L <hadi_l.@163.com>
2020-09-02 15:01:50 +0800

    6

--- src/main/java/Hadi/domain/Person.java
+++ src/main/java/Hadi/domain/Person.java
@@ -3,46 +3,5 @@ package Hadi.domain;
 public class Person {
     private String name;
     private Integer id;
-    private String address;
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public Person() {
-    }
-
-    public Person(String name, Integer id) {
-        this.name = name;
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", address='" + address + '\'' +
-                '}';
-    }
 }
